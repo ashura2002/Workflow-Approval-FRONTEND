f006b2beb40f1185448fee3abf265e45ff54574f
Jmark Dayna <jemarcdayna352@gmail.com>
2026-04-19 01:37:45 +0800

    fix cors origin error

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_BASE_URL || "http://localhost:8000";
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: baseURL,
 });
 
 // Request interceptor to attach token to headers
