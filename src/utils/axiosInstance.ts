59bc9d86e9dd384fc2820301d6b66e13ab0995ed
Jmark Dayna <jemarcdayna352@gmail.com>
2026-04-20 13:14:55 +0800

    test solution for cors error on deployment

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://approval-workflow-system.vercel.app',
+  baseURL: "https://approval-workflow-system.vercel.app",
   timeout: 10000,
 });
 
