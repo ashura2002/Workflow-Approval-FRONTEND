6d07bf5a4382ea5b5cc3f1e34adfddd7392ff4d2
Jmark Dayna <jemarcdayna352@gmail.com>
2026-04-20 13:26:18 +0800

    kapoya

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://approval-workflow-system.vercel.app",
+  baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 10000,
 });
 
